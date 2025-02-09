c543633d5cd0618a11adb7f32078f05aa34d40c4
Jochen <rothjochen@gmail.com>
2025-02-09 22:35:41 +0100

    Add TLS configuration

--- rootfs/etc/cont-init.d/miqro.sh
+++ rootfs/etc/cont-init.d/miqro.sh
@@ -15,17 +15,28 @@ else
     bashio::log.error "Device NOT found: $(bashio::config 'serial_device')"
 fi
 
+if [[ -z "$(bashio::config 'use_tls')" ]]; then
+    bashio::log.info "TLS enabled - certs are store in /ssl/<filename>"
+fi
+
 # Get configuration values and write /etc/miqro.yml configuration
 bashio::var.json \
   log_level "$(bashio::config 'log_level')" \
   host "$(bashio::config 'host')" \
-  port "$(bashio::config 'port')" \
+  port "^$(bashio::config 'port')" \
   username "$(bashio::config 'username')" \
   password "$(bashio::config 'password')" \
-  keepalive "$(bashio::config 'keepalive')" \
+  keepalive "^$(bashio::config 'keepalive')" \
   serial_device "$(bashio::config 'serial_device')" \
-  baudrate "$(bashio::config 'baudrate')" \
+  baudrate "^$(bashio::config 'baudrate')" \
   timeout "$(bashio::config 'timeout')" \
+  use_tls "^$(bashio::config 'use_tls')" \
+  ca_certs "$(bashio::config 'ca_certs')" \
+  certfile "$(bashio::config 'certfile')" \
+  keyfile "$(bashio::config 'keyfile')" \
+  cert_reqs "$(bashio::config 'cert_reqs')" \
+  tls_version "$(bashio::config 'tls_version')" \
+  ciphers "$(bashio::config 'ciphers')" \
   | tempio \
     -template /usr/share/tempio/miqro.gtpl \
     -out /etc/miqro.yml
--- rootfs/usr/share/tempio/miqro.gtpl
+++ rootfs/usr/share/tempio/miqro.gtpl
@@ -14,3 +14,13 @@ auth:
   username: "{{ .username }}"
   password: "{{ .password }}"
 {{end}}
+
+{{ if eq true .use_tls }}
+tls:
+  ca_certs: /ssl/{{ .ca_certs }}
+  certfile: /ssl/{{ .certfile }}
+  keyfile: /ssl/{{ .keyfile }}
+  cert_reqs: {{ .cert_reqs }}
+  tls_version: {{ .tls_version }}
+  ciphers: {{ .ciphers }}
+{{end}}
